17a289d21b1a486f1e6a643fd5270dc3c4030a8c
rathlucas <lucin189@gmail.com>
2021-03-03 00:05:50 -0300

    Add files via upload

--- idlev1.py
+++ idlev1.py
@@ -14,16 +14,23 @@ sleep(1)
 print('Bem-vindo à matrix! ')
 sleep(1)
 print('Antes de começar, realize uma decisão crucial... ')
-escolha = str(input('Em uma situação hipotética em que duas pílulas de cores diferentes estão dispostas lado a lado em uma mesa diretamente na sua frente, você escolhe (AZUL) ou (VERMELHO): ')).upper().strip()
+escolha = str(input('\nEm uma situação hipotética em que duas pílulas de cores diferentes estão dispostas lado a lado em uma mesa diretamente na sua frente, você escolhe \033[34m(AZUL)\033[m ou \033[31m(VERMELHO):\033[m ')).upper().strip()
 azul = 0
 vermelho = 0
+retry = ' '
 if escolha == 'AZUL':
 	azul += 1
-elif escolha == 'VERMELHO':
+while escolha != 'VERMELHO' != 'AZUL':
+	retry = str(input('Opção inválida! Digite vermelho ou azul: '))
+if escolha == 'VERMELHO':
 	vermelho += 1
 while vermelho != 0 and vermelho < 10:
-	print('!!!FALHA NA MATRIX!!!')
+	print('\n!!!FALHA NA MATRIX!!!')
 	vermelho += 1
+if vermelho > 1:
+	print('\n\033[31mSábia escolha, jovem guerreiro...\033[m')
+
+
 	
 	
 	
